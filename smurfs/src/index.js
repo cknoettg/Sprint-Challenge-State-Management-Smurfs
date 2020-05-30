145dac4cf502688f973a7de2a908e7db639c69cf
cknoettg <cknoettg@yahoo.com>
2020-05-29 21:33:42 -0400

    Skeleton file structure

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -4,7 +4,10 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
+import { smurfReducer } from './reducers/smurfReducer';
 import "./index.css";
 import App from "./components/App";
+
+const store = createStore(smurfReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
