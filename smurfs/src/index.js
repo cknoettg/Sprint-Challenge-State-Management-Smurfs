7fbb8ce6bff2923271f3dcd53b695e3361702a50
cknoettg <cknoettg@yahoo.com>
2020-05-29 21:29:36 -0400

    Initial commit with Answers.md filled out

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
